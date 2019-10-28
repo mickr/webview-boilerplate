b55c60f442863a3840db1f3c320aad8facb6617c
Mick Ryan <mickryan@box.com>
2019-10-28 09:45:00 -0700

    Adding webView changes and add property for accessing insecure sites.

--- webViewBoilerplate/ViewController.swift
+++ webViewBoilerplate/ViewController.swift
@@ -1,20 +1,39 @@
 //
 //  ViewController.swift
-//  webViewBoilerplate
 //
-//  Created by Mick Ryan on 10/28/19.
-//  Copyright © 2019 Box. All rights reserved.
+//  Created by Mick Ryan on 10/23/19.
+//  Copyright © 2019 Mick Ryan. All rights reserved.
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "http://google.com")!
+        webView.load(URLRequest(url: url))
+        
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
 }
+
 
